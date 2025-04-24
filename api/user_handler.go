25d8f3d3f734d900169f2b9d2891bdbb6157a95f
pedram <peeeedii@gmail.com>
2025-04-24 21:01:21 +0330

    refactor: sort the db, handler and another stuff in a good place

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,14 +1,35 @@
 package api
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/pdrm26/hotel-reservation/db"
 	"github.com/pdrm26/hotel-reservation/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
-	return c.JSON(types.User{FirstName: "Pedram Baradarian", LastName: "Jamshid rostami"})
+type UserHandler struct {
+	userStore db.UserStore
 }
 
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"user": "Pedram Baradarian"})
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	return c.JSON(types.User{FirstName: "Pedram Baradarian", LastName: "Jamshid rostami"})
 }
--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
+
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/pdrm26/hotel-reservation/api"
+	"github.com/pdrm26/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	// uri := os.Getenv("MONGODB_URI")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
+	// apiv1.Get("/user", api.HandleGetUsers)
 
 	app.Listen(*listenAddr)
 }
