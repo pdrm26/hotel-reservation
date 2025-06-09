27f18f794a7824b88fc92ff22dd2ce428757cab5
pedram <peeeedii@gmail.com>
2025-06-09 23:59:25 +0330

    feat: get the authentication user info

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt"
 	"github.com/pdrm26/hotel-reservation/db"
@@ -10,6 +12,11 @@ type AuthHandler struct {
 	userStore db.UserStore
 }
 
+type AuthParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func NewAuthHandler(userStore db.UserStore) *AuthHandler {
 	return &AuthHandler{
 		userStore: userStore,
@@ -17,6 +24,12 @@ func NewAuthHandler(userStore db.UserStore) *AuthHandler {
 }
 
 func (h *UserHandler) HandleAuthenticate(c *fiber.Ctx) error {
+	var AuthParams AuthParams
+	if err := c.BodyParser(&AuthParams); err != nil {
+		return err
+	}
+
+	fmt.Println(AuthParams, "**************************")
 	t := jwt.New(jwt.SigningMethodES256)
 	s, err := t.SigningString()
 	if err != nil {
