6ae0637eddc10fe6825ab38e5b06161344fcf330
pedram <peeeedii@gmail.com>
2025-06-27 01:12:26 +0330

    chore: change the var test db name for more clarity

--- db/db.go
+++ db/db.go
@@ -2,7 +2,7 @@ package db
 
 const DBURI = "mongodb://localhost:27017"
 const DBNAME = "hotel-reservation"
-const DBNAMETEST = "hotel-reservation-test"
+const TestDBName = "hotel-reservation-test"
 
 type Store struct {
 	User    UserStore
